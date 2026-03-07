34ca07e6bbfc8d92c648a3ef034b8d2fd47366e4
Nicole-Chen97 <74529722+Nicole-Chen97@users.noreply.github.com>
2026-03-07 08:29:28 +0000

    pipeline code working

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -5,7 +5,8 @@ print(f'Running pipeline for {month}')
 
 
 import pandas as pd 
-df = pd.DataFrame ({"A":[1,2] , "B":[3,4]})
+df = pd.DataFrame ({"day":[1,2] , "num_passengers":[3,4]})
+df['month'] = month 
 print(df.head())
 
 df.to_parquet(f'output_{month}.parquet')
